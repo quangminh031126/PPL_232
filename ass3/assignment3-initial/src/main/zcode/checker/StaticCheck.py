d1319185435fb1cb720b28e907719bbe917691d4
Minh <minh.pham1126@gmail.com>
2024-04-15 13:29:36 +0700

    ass3: init changes

--- ass3/assignment3-initial/src/main/zcode/checker/StaticCheck.py
+++ ass3/assignment3-initial/src/main/zcode/checker/StaticCheck.py
@@ -6,4 +6,74 @@ from functools import reduce
 
 
 class StaticChecker(BaseVisitor, Utils):
-    pass
+    def visitProgram(self, ast, param):
+        pass
+
+    def visitVarDecl(self, ast, param):
+        pass
+
+    def visitFuncDecl(self, ast, param):
+        pass
+
+    def visitNumberType(self, ast, param):
+        return NumberType()
+
+    def visitBoolType(self, ast, param):
+        return BoolType()
+
+    def visitStringType(self, ast, param):
+        return StringType()
+
+    def visitArrayType(self, ast, param):
+        return ArrayType()
+
+    def visitBinaryOp(self, ast, param):
+        pass
+
+    def visitUnaryOp(self, ast, param):
+        pass
+
+    def visitCallExpr(self, ast, param):
+        pass
+
+    def visitId(self, ast: Id, param):
+        pass
+
+    def visitArrayCell(self, ast, param):
+        pass
+
+    def visitBlock(self, ast, param):
+        pass
+
+    def visitIf(self, ast, param):
+        pass
+
+    def visitFor(self, ast, param):
+        pass
+
+    def visitContinue(self, ast, param):
+        pass
+
+    def visitBreak(self, ast, param):
+        pass
+
+    def visitReturn(self, ast, param):
+        pass
+
+    def visitAssign(self, ast, param):
+        pass
+
+    def visitCallStmt(self, ast, param):
+        pass
+
+    def visitNumberLiteral(self, ast: NumberLiteral, param):
+        pass
+
+    def visitBooleanLiteral(self, ast: BooleanLiteral, param):
+        return BoolType()
+
+    def visitStringLiteral(self, ast: StringLiteral, param):
+        return StringType()
+
+    def visitArrayLiteral(self, ast: ArrayType, param):
+        return ArrayType(ast.size,ast.eleType)
